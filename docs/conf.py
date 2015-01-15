2aad8fbebdf3e6d855b2c5ed5b42457d847098ec
Ryan Dwyer <ryanpdwyer@gmail.com>
2015-01-15 13:59:30 -0500

    Added local rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 import sys
 import os
 
-# Check if on READTHEDOCS. If so, we need to mock out our c dependencies
+# Check if on READTHEDOCS. If so, we need to mock out C-dependent packages
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # See http://goo.gl/GVB1Vu
@@ -134,9 +134,15 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ---------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'default'
+# The theme to use for HTML and HTML Help pages.
+if not on_rtd:
+    # only import and set the theme if we're building docs locally
+    # otherwise, readthedocs.org uses their theme by default,
+    # so no need to specify it
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
