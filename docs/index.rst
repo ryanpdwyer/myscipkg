beec73323a566717f99c5fc361b1005dce259d74
Ryan Dwyer <ryanpdwyer@gmail.com>
2015-01-15 14:20:58 -0500

    Added autodoc of myscipkg

--- docs/index.rst
+++ docs/index.rst
@@ -22,6 +22,7 @@ Contents:
    installation
    contributing
    authors
+   myscipkg
 
 Feedback
 ========
--- myscipkg/__init__.py
+++ myscipkg/__init__.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+r"""
+========
+myscipkg
+========
+
+Here is some documentation parsed by ``autodoc``.
+We should be able to include math,
+
+.. math::
+    
+    E = \frac{1}{2} m v^2 + \frac{1}{2} k x^2
+
+and images,
+
+.. image:: images/ex.png
+
+as normally.
+"""
 __author__ = 'Ryan Dwyer'
 __email__ = 'ryanpdwyer@gmail.com'
 __version__ = '0.1.0'
